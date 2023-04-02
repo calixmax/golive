e7ce91bb189ede6b109fce756f445d2ddcd026d5
calixmax <129668248+calixmax@users.noreply.github.com>
2023-04-02 13:26:32 +0000

    Update lightfatv.js

--- www.lightfatv.com/lightfatv.js
+++ www.lightfatv.com/lightfatv.js
@@ -10,7 +10,7 @@
 			player = new YT.Player('ytplayer', {
 				height: '360',
 				width: '640',
-				videoId: 'einm17_qK3A',
+				videoId: 'EZdwMqnNHE8',
 				playerVars: {
   autoplay: 1,
   controls: 1,
